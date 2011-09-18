514625e541a29553949abd10b32a230571f5aeca
maemo <maemo@maemo-TomXue>
2011-09-18 08:05:51 +0100

    modify the tcpclient.c so that it acts like ./tcpclient 192.168.0.100, modify the both tcp source files with better format

--- udpserver.c
+++ udpserver.c
@@ -1,20 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  udpserver.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  09/15/11 03:14:04
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Tom Xue (), tom.xue@nokia.com
- *        Company:  Nokia
- *
- * =====================================================================================
- */
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
@@ -27,41 +10,46 @@
 
 void do_echo(int sockfd, struct sockaddr *pcliaddr, socklen_t clilen)
 {
-int n;
-socklen_t len;
-char mesg[MAXLINE];
+    int n;
+    socklen_t len;
+    char mesg[MAXLINE];
 
-for(;;)
-{
-len = clilen;
-/* waiting for receive data */
-n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
-/* sent data back to client */
-sendto(sockfd, mesg, n, 0, pcliaddr, len);
-}
+    for(;;)
+    {
+        len = clilen;
+        /* waiting for receiving data */
+        n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+        /* sent data back to client */
+        sendto(sockfd, mesg, n, 0, pcliaddr, len);
+    }
 }
 
 int main(void)
 {
-int sockfd;
-struct sockaddr_in servaddr, cliaddr;
+    int sockfd;
+    struct sockaddr_in servaddr, cliaddr;
 
-sockfd = socket(AF_INET, SOCK_DGRAM, 0); /* create a socket */
+    //SOCK_DGRAM: Connectionless, unreliable datagrams of fixed maximum length.
+    //IPPROTO_UDP = 17, User Datagram Protocol.
+    //IPPROTO_IP = 0, Dummy protocol for TCP.
+    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); /* create a socket */
 
-/* init servaddr */
-bzero(&servaddr, sizeof(servaddr));
-servaddr.sin_family = AF_INET;
-servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-servaddr.sin_port = htons(SERV_PORT);
+    /* init servaddr */
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    //Convert the host byte order to network byte order
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
 
-/* bind address and port to socket */
-if(bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
-{
-perror("bind error");
-exit(1);
+    /* bind address and port to socket */
+    if(bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
+    {
+        perror("bind error");
+        exit(1);
+    }
+
+    do_echo(sockfd, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
+
+    return 0;
 }
 
-do_echo(sockfd, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
-
-return 0;
-}
